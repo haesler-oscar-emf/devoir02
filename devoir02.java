f80300207b1e9b800243f3bc972e045d316f4e79
haesler-oscar-emf <oscar.haesler@studentfr.ch>
2023-09-15 12:33:55 +0200

    devoir02

--- devoir02.java
+++ devoir02.java
@@ -5,7 +5,6 @@ public class devoir02 {
     public static final int CAPACITE_RESERVOIR_B = 5;
     public static void main(String[] args) {
 
- 
 
         int reservoirA = 0;
         int reservoirB = 0;
